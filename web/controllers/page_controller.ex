9b0770dded001c04fd2ff96d3a8e8a621814ba78
mazaz <mark@f6s.com>
2018-03-23 16:38:31 +0000

    pry working controller

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,12 @@
+
+require IEx
+
 defmodule AppToLookAtPhoneixElixir.PageController do
   use AppToLookAtPhoneixElixir.Web, :controller
 
   def index(conn, _params) do
+  	#IEx.pry
+  	#request_heads = conn.req_headers
     render conn, "index.html"
   end
 end
--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,4 @@
 defmodule AppToLookAtPhoneixElixir.PageView do
   use AppToLookAtPhoneixElixir.Web, :view
+
 end
